2825a0c8fcb45bdc0c00925d0f2e7c2d189b8c47
Anmol Dash <anmold@sas.upenn.edu>
2025-03-11 12:01:06 -0500

    fl8

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
 
+// Redirect non-hash URLs to hash-based ones
+if (!window.location.hash) {
+  const path = window.location.pathname + window.location.search;
+  window.location.replace('#' + path);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
