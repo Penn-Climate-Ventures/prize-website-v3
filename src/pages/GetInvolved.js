d5c723838d7f7d0ffa8ce6e2573555fa62eaa0c7
Anmol Dash <anmold@sas.upenn.edu>
2025-12-29 00:45:32 +0200

    test3

--- src/pages/GetInvolved.js
+++ src/pages/GetInvolved.js
@@ -26,7 +26,7 @@ const GetInvolved = () => {
           <div className="content">
             <p>
               Penn Climate Ventures is always looking for new sponsors and judges to help support
-              our mission.
+              our mission. Please select an option below:
             </p>
           </div>
         </section>
@@ -76,33 +76,9 @@ const GetInvolved = () => {
                 serving as a judge, you'll empower student entrepreneurs to bring their ideas to
                 life.
               </p>
-              <p>
-                Reach out to prizepcv@gmail.com if you are interested in becoming a judge or mentor
-                for next year's competition. We are recruiting judges for each of the three rounds:
-                written application, slide deck, and live pitch judging.
-              </p>
-              <br />
-              <h3>Final Round Judges:</h3>
-              <div className="judges-list">
-                <div className="judge-item">
-                  <img src={`${process.env.PUBLIC_URL}/final-round-judges/annelise-dyer.jpeg`} alt="Anneliese Dyer" className="judge-photo" />
-                  <p>Anneliese Dyer, 4AIR</p>
-                </div> 
-                <div className="judge-item">
-                  <img src={`${process.env.PUBLIC_URL}/final-round-judges/david-jacoby.jpeg`} alt="David Steven Jacoby" className="judge-photo" />
-                  <p>David Steven Jacoby, NYU Stern Business School</p> 
-                </div>
-                <div className="judge-item">
-                  <img src={`${process.env.PUBLIC_URL}/final-round-judges/daniel-bloch.jpeg`} alt="Daniel Bloch" className="judge-photo" />
-                  <p>Daniel Bloch, Lanzajet</p>
-                </div>
-                <div className="judge-item">
-                  <img src={`${process.env.PUBLIC_URL}/final-round-judges/chuck-hurchalla.jpeg`} alt="Chuck Hurchalla" className="judge-photo" />
-                  <p>Chuck Hurchalla, Evolution Sustainability Group</p>
-                </div>
-              </div>
-              <br />
-              <h3>We would like to thank our Round 1 Judges for their hard work grading the pitches. We could not have done this without their respective expertise in the field. Thank you to:</h3>
+              <p>Reach out to prizepcv@gmail.com if you are interested in becoming a judge or mentor for next year's competition. We are recruiting judges for each of the three rounds: written application, slide
+              deck, and live pitch judging.</p>
+              <p>We would like to thank our Round 1 Judges for their hard work grading the pitches. We could not have done this without their respective expertise in the field. Thank you to: </p>
               <ul>
                 <li>Siddharth Gundala - Columbia University, Master's Student in Climate and Society</li>
                 <li>Julia Campbell - University of Michigan, PhD Student in Climate Science</li>
@@ -122,7 +98,6 @@ const GetInvolved = () => {
                 <li>Alexandra Gonzalez - Columbia University, Master's Student in Climate and Society, Policy</li>
                 <li>Molly Fowler - University of Pennsylvania, Master's Student in Environmental Studies</li>
               </ul>
-              <br />
               {/* <p><strong>Judge List:</strong></p>
               <p>Photos and descriptions of judges will be added here.</p> */}
               <br /><br />
